1c135260eb0692a29381b156ce19d557cdae0575
Stévillis Sousa <stevillis@hotmail.com>
2024-03-24 14:01:52 -0400

    feat: add go to index input

--- data_preparation/annotation_tool.py
+++ data_preparation/annotation_tool.py
@@ -11,8 +11,36 @@ import streamlit as st
 
 SENTIMENT_DICT = {"Positive": 1, "Negative": -1, "Neutral": 0}
 
+CUSTOM_CSS = """
+<style>
+[data-testid="stApp"] {
+  // background-color:red;
+}
+
+div[data-testid="stHorizontalBlock"] {
+  // background-color: yellow;
+  align-items: end;
+}
+
+div[data-testid="stHorizontalBlock"] div[data-testid="stButton"]{
+  // background-color: blue;
+  display: flex;
+  justify-content: end;
+}
+</style>
+"""
+
+
+def go_to_page():
+    st.session_state["index"] = st.session_state["go_to_page_input"]
+
 
 if __name__ == "__main__":
+    st.markdown(
+        CUSTOM_CSS,
+        unsafe_allow_html=True,
+    )
+
     st.title("Sentiment Annotation Tool")
 
     uploaded_file = st.file_uploader("Choose a file", type=[".csv"])
@@ -21,53 +49,49 @@ if __name__ == "__main__":
         st.session_state["index"] = 0
 
     if uploaded_file is not None:
-        # st.write(uploaded_file)
-
-        if st.button("Download file"):
-            file_name, file_extension = uploaded_file.name.split(".")
-            date_time_str = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
-
-            reviews_df = st.session_state.get("reviews_df")
-            reviews_df.to_csv(
-                f"reviewed_{file_name}_{date_time_str}.{file_extension}",
-                index=False,
-            )
-
         if "reviews_df" not in st.session_state:
             reviews_df = pd.read_csv(uploaded_file)
             st.session_state["reviews_df"] = reviews_df
 
         reviews_df = st.session_state.get("reviews_df")
 
-        col_predicted_label, col_predicted_score = st.columns(2)
-        with col_predicted_label:
-            st.text(
-                f"Predicted Label: {reviews_df.iloc[st.session_state['index']]['predicted_label']}"
-            )
+        col_index, _, col_download_file = st.columns(3)
+        with col_index:
+            st.write("Index: " + str(st.session_state["index"]))
 
-        with col_predicted_score:
-            st.text(
-                f"Predicted Score: {reviews_df.iloc[st.session_state['index']]['predicted_score']}"
-            )
+        with col_download_file:
+            if st.button("Download file"):
+                file_name, file_extension = uploaded_file.name.split(".")
+                date_time_str = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
 
-        new_label = st.selectbox(
-            label="Change Label",
-            options=list(SENTIMENT_DICT.keys()),
-            key="label",
+                reviews_df = st.session_state.get("reviews_df")
+                reviews_df.to_csv(
+                    f"reviewed_{file_name}_{date_time_str}.{file_extension}",
+                    index=False,
+                )
+
+        st.number_input(
+            label="Go to Index:",
+            step=1,
+            value=st.session_state["index"],
+            min_value=0,
+            max_value=reviews_df.shape[0] - 1,
+            key="go_to_page_input",
+            on_change=go_to_page,
         )
 
-        col_previous, col_update, col_next = st.columns(3)
-        with col_previous:
-            if st.button(
-                label="Previous",
-                # disabled=st.session_state["index"] == 0,
-                key="previous",
-            ):
-                if st.session_state["index"] > 0:
-                    st.session_state["index"] -= 1
+        col_new_label, col_update_label = st.columns(2)
+        with col_new_label:
+            new_label = st.radio(
+                label="New Label",
+                options=list(SENTIMENT_DICT.keys()),
+                key="new_label",
+                horizontal=True,
+                label_visibility="hidden",
+            )
 
-        with col_update:
-            if st.button("Update", key="update"):
+        with col_update_label:
+            if st.button("Update Label", key="update_label"):
                 reviews_df.at[st.session_state["index"], "sentiment"] = SENTIMENT_DICT[
                     new_label
                 ]
@@ -76,26 +100,26 @@ if __name__ == "__main__":
                 st.session_state["index"] += 1
                 st.session_state["reviews_df"] = reviews_df
 
-        with col_next:
-            if st.button(
-                "Next",
-                # disabled=st.session_state["index"] == reviews_df.shape[0],
-                key="next",
-            ):
-                if st.session_state["index"] < reviews_df.shape[0]:
-                    st.session_state["index"] += 1
-
-        with st.container(border=True, height=320):
+        with st.container(border=True, height=240):
             st.write("#### Review Text")
             st.write(f"{reviews_df.iloc[st.session_state['index']]['review_text']}")
 
-        col_index, col_reviewed = st.columns(2)
-        with col_index:
-            st.write("Index: " + str(st.session_state["index"]))
+        col_current_label, col_predicted_label, col_predicted_score = st.columns(3)
+        with col_current_label:
+            list_sentiment_dict_keys = list(SENTIMENT_DICT.keys())
+            list_sentiment_dict_values = list(SENTIMENT_DICT.values())
 
-        with col_reviewed:
-            if "reviewed" in reviews_df:
-                st.write(
-                    "Index: "
-                    + str(reviews_df.at[st.session_state["index"], "reviewed"])
-                )
+            sentiment = reviews_df.iloc[st.session_state["index"]]["sentiment"]
+            sentiment_position = list_sentiment_dict_values.index(sentiment)
+
+            st.text(f"Current Label: {list_sentiment_dict_keys[sentiment_position]}")
+
+        with col_predicted_label:
+            st.text(
+                f"Predicted Label: {reviews_df.iloc[st.session_state['index']]['predicted_label']}"
+            )
+
+        with col_predicted_score:
+            st.text(
+                f"Predicted Score: {reviews_df.iloc[st.session_state['index']]['predicted_score']:.4f}"
+            )
